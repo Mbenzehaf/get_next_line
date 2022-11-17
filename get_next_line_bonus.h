e9346623a7ca8b751cc9aa8463846ba978b719be
Mohammed Ben Zehaf <mben-zeh@e2r10p3.1337.ma>
2022-11-17 12:00:34 +0100

    get_next_line

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mben-zeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/07 03:48:28 by mben-zeh          #+#    #+#             */
+/*   Updated: 2022/11/09 23:52:23 by mben-zeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <fcntl.h>
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_strlen(const char *str);
+int		ft_strlen_n(const char *str, char c);
+char	*ft_strdup(const char *s1);
+int		ft_strfind(const char *s);
+#endif
